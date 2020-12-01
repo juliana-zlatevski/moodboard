a071c7c72e7e7502a51c0209e6648712b0536a33
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-11-30 22:00:00 -0600

    adds post index page

--- posts/views.py
+++ posts/views.py
@@ -3,10 +3,20 @@ from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Post
 
-# Create your views here.
+# home page
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={})
 
+# post tweet view
+def posts_list_view(request, *args, **kwargs):
+    allPosts = Post.objects.all()
+    posts_list = [{"id": post.id, "content": post.content} for post in allPosts]
+    data = {
+      "response": posts_list
+    }
+    return JsonResponse(data)
+
+# post detail view
 def post_detail_view(request, post_id, *args, **kwargs):
     data = {
       "id": post_id,
