6a8c344d3fdf664214e39c69024ace4cc86b796c
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-11-30 21:49:34 -0600

    adds templates directory & base.html file. brings in bootstrap.

--- posts/views.py
+++ posts/views.py
@@ -1,15 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.http import HttpResponse, Http404, JsonResponse
 
 from .models import Post
 
 # Create your views here.
 def home_view(request, *args, **kwargs):
-  return HttpResponse("<h1>Home View</h1>")
+    return render(request, "pages/home.html", context={})
 
 def post_detail_view(request, post_id, *args, **kwargs):
-  try:
-    obj = Post.objects.get(id=post_id)
-  except:
-    raise Http404
-  return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
+    data = {
+      "id": post_id,
+    }
+    status = 200
+    try:
+        obj = Post.objects.get(id=post_id)
+        data['content'] = obj.content
+    except:
+        data['message'] = "Post not found"
+        status = 404
+    return JsonResponse(data, status=status)
