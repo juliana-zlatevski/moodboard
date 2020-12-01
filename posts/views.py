f62be56e36b6b1534d4b48579e2a33e06a4d5c01
juliana-zlatevski <juliana.zlatevski@hotmail.com>
2020-11-30 21:25:04 -0600

    maps home and post detail urls

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
+
+from .models import Post
 
 # Create your views here.
+def home_view(request, *args, **kwargs):
+  return HttpResponse("<h1>Home View</h1>")
+
+def post_detail_view(request, post_id, *args, **kwargs):
+  try:
+    obj = Post.objects.get(id=post_id)
+  except:
+    raise Http404
+  return HttpResponse(f"<h1>Hello {post_id} - {obj.content}</h1>")
